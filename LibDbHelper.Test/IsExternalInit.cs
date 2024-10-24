adcbb93ead0cdf44ff7b004a967b9b1977565e8b
kitada <taihei.kitada@gmail.com>
2024-10-24 21:47:15 +0900

    GetConnectionの引数削除とGetCommandの引数connection削除 close #16
    - ついでにIsExternalInitの警告抑制

--- LibDbHelper.Test/IsExternalInit.cs
+++ LibDbHelper.Test/IsExternalInit.cs
@@ -1,4 +1,8 @@
-﻿namespace System.Runtime.CompilerServices
+﻿#pragma warning disable IDE0130 // Namespace がフォルダー構造と一致しません
+namespace System.Runtime.CompilerServices
+#pragma warning restore IDE0130 // Namespace がフォルダー構造と一致しません
 {
-    public static class IsExternalInit { }
+    public static class IsExternalInit
+    {
+    }
 }
