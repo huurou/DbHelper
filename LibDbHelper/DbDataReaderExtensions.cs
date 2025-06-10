d6b407403b3630fda596ca9241ab888412889ed1
kitada <taihei.kitada@gmail.com>
2025-06-10 20:19:40 +0900

    GetStringNullable追加
    ついでにGetDateTimeNullable修正

--- LibDbHelper/DbDataReaderExtensions.cs
+++ LibDbHelper/DbDataReaderExtensions.cs
@@ -14,6 +14,17 @@ namespace LibDbHelper
         public static string GetString(this DbDataReader reader, string columnName)
         {
             var ordinal = reader.GetOrdinal(columnName);
+            return reader.GetString(ordinal);
+        }
+
+        public static string GetStringNullable(this DbDataReader reader, string columnName)
+        {
+            var ordinal = reader.GetOrdinal(columnName);
+            return reader.GetStringNullable(ordinal);
+        }
+
+        public static string GetStringNullable(this DbDataReader reader, int ordinal)
+        {
             return reader.IsDBNull(ordinal) ? null : reader.GetString(ordinal);
         }
 
@@ -76,18 +87,17 @@ namespace LibDbHelper
             var ordinal = reader.GetOrdinal(columnName);
             return reader.IsDBNull(ordinal) ? throw new InvalidOperationException($"値がDBNullでした columnName:{columnName}") : reader.GetDateTime(ordinal);
         }
-        
+
         /// <summary>
         /// 指定された列名の値をDateTime?として取得します。
         /// </summary>
         /// <param name="reader">DbDataReader継承クラスのインスタンス</param>
         /// <param name="columnName">列名</param>
         /// <returns></returns>
-        public static DateTime GetDateTimeNullable(this DbDataReader reader, string columnName)
+        public static DateTime? GetDateTimeNullable(this DbDataReader reader, string columnName)
         {
             var ordinal = reader.GetOrdinal(columnName);
-            return reader.IsDBNull(ordinal) ? throw new InvalidOperationException($"値がDBNullでした columnName:{columnName}") : reader.GetDateTime(ordinal);
+            return reader.IsDBNull(ordinal) ? (DateTime?)null : reader.GetDateTime(ordinal);
         }
-
     }
 }
